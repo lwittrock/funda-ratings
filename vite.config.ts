d6f9ed951c18c83998c53b797ab98c933039cd0c
Lars Wittrock <larwitt@gmx.de>
2025-12-28 17:32:16 +0100

    Fix path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/funda-ratings/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
